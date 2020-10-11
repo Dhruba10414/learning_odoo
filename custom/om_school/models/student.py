d38df708015c8e768521600ce6cf5b704d7e27d3
dhruba10414 <dhruba10414@gmail.com>
2020-10-11 19:57:45 +0600

    adv orm, api add

--- custom/om_school/models/student.py
+++ custom/om_school/models/student.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.exceptions import UserError, ValidationError
 
 class SchoolStudent(models.Model):
     _name = 'school.student'
@@ -15,3 +16,23 @@ class SchoolStudent(models.Model):
         ('other', 'Other'),
     ], string='Gender', default='male')
     image = fields.Binary(string="Image")
+
+    tuition_fee = fields.Float(string='Tuition Fee', default= 0.0)
+
+    additional_donation= fields.Float(string='Additional Donation', default=10000.0)
+
+    total_fee= fields.Float(string='Total Fees', readonly= True)
+
+    @api.onchange('tuition_fee','additional_donation')
+    def _onchnge_total_fee(self):
+        if self.tuition_fee < 0.00:
+            raise UserError('Add a valid tuition fee.')
+
+        self.total_fee = self.tuition_fee + self.additional_donation
+
+    @api.constrains('additional_donation')
+    def _check_additional_donation(self):
+        for record in self:
+            if record.additional_donation < 10000:
+                raise ValidationError('Add a donation more than 10000.')
+
