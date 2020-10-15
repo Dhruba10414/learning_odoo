47bce575a63f00ab6585854e92d6dd0c5f54550e
dhruba10414 <dhruba10414@gmail.com>
2020-10-15 19:16:41 +0600

    calendar view, computed fileds

--- custom/om_school/models/session.py
+++ custom/om_school/models/session.py
@@ -1,4 +1,6 @@
-from odoo import models,api,fields
+from odoo import models,api,fields,exceptions
+from datetime import timedelta
+
 
 class SchoolSession(models.Model):
     _name = 'school.session'
@@ -14,3 +16,30 @@ class SchoolSession(models.Model):
     instructor_id = fields.Many2one(comodel_name='res.partner',string='Instructor')
 
     course_ids = fields.Many2many(comodel_name='res.partner',string='Courses')
+
+    start_date = fields.Date(string='Start Date',
+                             default=fields.Date.today)
+
+    duration = fields.Integer(string='Session Days',
+                              default=1)
+
+    end_date = fields.Date(string='End Date',
+                           compute='_compute_end_date',
+                           inverse='_inverse_end_date',
+                           store=True)
+
+    @api.depends('start_date', 'duration')
+    def _compute_end_date(self):
+        for record in self:
+            if not (record.start_date and record.duration):
+                record.end_date = record.start_date
+                continue
+            duration = timedelta(days=record.duration)
+            record.end_date = record.start_date + duration
+
+    def _inverse_end_date(self):
+        for record in self:
+            if record.start_date and record.end_date:
+                record.duration = (record.end_date - record.start_date).days + 1
+            else:
+                continue
